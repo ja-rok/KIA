0a15c0cd21073c49cd96181083de81da2d6750c1
ja-rok <s.karotsch@gmx.de>
2018-11-19 22:54:13 +0100

    Update Cards

--- src/components/kundenservice.vue
+++ src/components/kundenservice.vue
@@ -7,33 +7,48 @@
         <b-col cols=3>Suche</b-col>
         <b-col>
           <b-row>
-            <b-col cols=6>
-              <b-card title="Card Title"
+            <div class="d-flex flex-wrap">
+            
+            
+              <b-card     title="Card Title"
                   img-src="https://picsum.photos/600/300/?image=25"
                   img-alt="Image"
                   img-top
                   tag="article"
-                  style="max-width: 20rem;"
+                  style="max-width: 25rem;"
+
                   >
                       <p class="card-text">
                         Some quick example text to build on the card title and make up the bulk of the card's content.
                       </p>
                       <b-button href="#" variant="primary">Go somewhere</b-button>
             </b-card>
-          </b-col>
-            <b-col cols=6><b-card title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
+          
+            <b-card title="Card Title"
+                  img-src="https://picsum.photos/600/300/?image=25"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 25rem;"
           >
     <p class="card-text">
       Some quick example text to build on the card title and make up the bulk of the card's content.
     </p>
     <b-button href="#" variant="primary">Go somewhere</b-button>
   </b-card>
-  </b-col>
+<b-card title="Card Title"
+                  img-src="https://picsum.photos/600/300/?image=25"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 25rem;"
+          >
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </p>
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+  </div>
           </b-row>
         </b-col>
     </b-row>
